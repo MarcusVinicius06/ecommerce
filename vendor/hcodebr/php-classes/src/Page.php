a1d2a9675c39806c4945c44e6fa6b5817ac7c7a1
Marcus Vinicius <marcusviniciusdasilva6@gmail.com>
2019-04-17 11:00:36 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -12,6 +12,8 @@ class Page
   private $tpl;
   private $options = [];
   private $defaults = [
+    "header"=>true,
+    "footer"=>true,
     "data" => []
   ];
 
@@ -31,7 +33,7 @@ class Page
 
     $this->setData($this->options["data"]);
 
-    $this->tpl->draw("header");
+    if ($this->options["header"] === true) $this->tpl->draw("header");
   }
 
   public function setData($data = array())
@@ -49,7 +51,7 @@ class Page
   }
 
   function __destruct(){
-    $this->tpl->draw("footer");
+    if ($this->options["footer"] === true) $this->tpl->draw("footer");
   }
 }
 
